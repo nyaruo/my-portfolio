899c7b695620395e1d7ff326fd1e6dc07e00a6e5
nyaruo <tanikawanko@gmail.com>
2021-10-17 06:33:01 +0900

    change style sukoshi

--- components/Activity.js
+++ components/Activity.js
@@ -4,7 +4,7 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 export default function Activity() {
     return (
         <div className="block mt-5 ml-3 mr-3 pt-4 pl-4 pb-6 bg-white border-2 border-gray-100 rounded">
-            <h1 className="text-3xl p-2">Activity(活動)</h1>
+            <h1 className="text-3xl p-2 ">Activity(活動)</h1>
             <div className="md:flex md:space-x-3 mr-3 ml-3">
                 <div className="mt-3">
                     <div className="block m-1 p-5 border-2 border-gray-100 rounded mt-3">
@@ -19,7 +19,7 @@ export default function Activity() {
                             <p>技術、ITニュースの共有、個人開発、共同開発を行なっています。</p>
                         </div>
                         <div className="md:flex md:space-x-3 mt-2 pb-2">
-                            <img className="pt-3"
+                            <img className="pt-3 "
                                 src="/semi-activity1.jpg"
                                 width={300}
                                 height={240}
@@ -41,7 +41,7 @@ export default function Activity() {
                         <h1 className="text-xl">趣味</h1>
                         <div className="text-base m-2">
                             <p>趣味はギターで、好きな食べ物は焼肉です。</p>
-                            <p>・　コロナ自粛期間に特技を身につけようと、ギターの練習を始めました。</p>
+                            <p>・　コロナ自粛期間に特技を身につける為、ギターの練習を始めました。</p>
                             <p>・　焼肉が大好きで、2年以上焼肉屋でアルバイトをしています。</p>
                             <div className="md:flex md:space-x-3 mt-2">
                                 <img className="md:w-56 pt-3"
