64b2dd000e12ed979301cb20184c7ce6760a1faf
nyaruo <tanikawanko@gmail.com>
2021-10-17 22:41:34 +0900

    change style sukoshi

--- components/Article/Book/BookBox.js
+++ components/Article/Book/BookBox.js
@@ -1,7 +1,11 @@
 const BookBox = (props) => {
     return (
         <div className="block relative m-3 border-b-2 border-gray-100 p-3 pl-8">
-            <div className="text-sm"><a className="pr-36">{props.genre}</a><a>{props.title}</a></div>
+            <div className="text-sm">
+                <a className="pr-36">{props.genre}</a>
+                <a className="pr-48">{props.title}</a>
+                <a>{props.memo}</a>
+            </div>
         </div>
     )
 }
